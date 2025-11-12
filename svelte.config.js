697e1c27a745514ed860891a634eb5d5c842fe4c
Yaroslav Ovdii <yaovdiy@gmail.com>
2025-11-12 15:56:20 +0700

    config: added removete functions to the kit

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,18 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		experimental: {
+			remoteFunctions: true
+		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
+	},
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
 	}
 };
 
