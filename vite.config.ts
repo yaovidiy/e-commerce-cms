f6311c616f0e190540b13e6149f62e497225964a
Yaroslav Ovdii <yaovdiy@gmail.com>
2025-11-12 19:03:20 +0700

    migrated tailwind to v4 added shadcn svelte

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
 		paraglide({
 			project: './project.inlang',
